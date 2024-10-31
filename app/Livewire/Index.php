1559fd4e792aa0f9276140d3f461fe9178f7cea8
fco-assis-jr <franciscoassisjr00@gmail.com>
2024-10-31 16:33:48 -0400

    Alinhamento de Código

--- app/Livewire/Index.php
+++ app/Livewire/Index.php
@@ -2,20 +2,13 @@
 
 namespace App\Livewire;
 
-use Illuminate\Support\Facades\Session;
-use App\Models\Pclib;
-use Dflydev\DotAccessData\Data;
 use Livewire\Component;
-use Illuminate\Support\Facades\DB;
 use Jantinnerezo\LivewireAlert\LivewireAlert;
-use mysql_xdevapi\Exception;
 
 
 class Index extends Component
 {
     use LivewireAlert;
-
-
 
     public function render()
     {
